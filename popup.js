fcafb20ea040b153fcfa1d92e8f6816e9849ee9c
Akira Cai <blackwindforce@users.noreply.github.com>
2025-12-28 05:28:57 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,25 +1,23 @@
-for (const { children: fs = [] } of await chrome.bookmarks.getSubTree("1")) {
-  for (const { children: bs = [] } of fs) {
-    const ss = new Set(
-      bs.toSorted(
-        (a, b) =>
-          new URL(a.url).origin.localeCompare(new URL(b.url).origin) ||
-          a.title.localeCompare(b.title, undefined, { numeric: true }),
-      ),
-    );
+const sort = (xs) =>
+  xs
+    .values()
+    .map((x) => x.children)
+    .filter(Boolean)
+    .map((ys) => ys.toSorted(compare))
+    .map((ys) => Array.fromAsync(ys, move))
+    .forEach((x) => x.then(sort));
 
-    const rs = new Set(
-      Map.groupBy(ss, (x) => x.title.replaceAll(/\d+/g, ""))
-        .values()
-        .flatMap((xs) => xs.slice(0, -1)),
-    );
+const compare = (a, b) => compareUrlOrigin(a, b) || compareTitle(a, b);
 
-    for (const x of ss.difference(rs)) {
-      await chrome.bookmarks.move(x.id, { parentId: x.parentId });
-    }
+const compareUrlOrigin = (a, b) =>
+  a.url && b.url && new URL(a.url).origin.localeCompare(new URL(b.url).origin);
 
-    for (const x of rs) {
-      chrome.bookmarks.remove(x.id);
-    }
-  }
-}
+const compareTitle = (a, b) =>
+  a.title.localeCompare(b.title, undefined, { numeric: true });
+
+const move = (x) => chrome.bookmarks.move(x.id, { parentId: x.parentId });
+
+(await chrome.bookmarks.getSubTree("1"))
+  .values()
+  .map((x) => x.children)
+  .forEach(sort);
